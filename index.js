21f91b8952bea27751e6e24b44a68eb10bb2bff5
KONSTANTINOS KOUKOS <kkonst2014@gmail.com>
2022-12-18 17:30:17 +0200

    moved categories to local, problem with closures TODO: FIX IT!

--- index.js
+++ index.js
@@ -4,12 +4,51 @@ const Spotify = require('spotify-web-api-node');
 const ytdl = require('ytdl-core');
 const { search } = require('yt-search');
 
+const categories = [{ id: 'toplists', name: 'Top Lists' },
+{ id: '0JQ5DAqbMKFzHmL4tf05da', name: 'Mood' },
+{ id: '0JQ5DAqbMKFEC4WFtoNRpw', name: 'Pop' },
+{ id: '0JQ5DAqbMKFQ00XGBls6ym', name: 'Hip-Hop' },
+{ id: '0JQ5DAqbMKFHOzuVTgTizF', name: 'Dance/Electronic' },
+{ id: '0JQ5DAqbMKFIVNxQgRNSg0', name: 'Decades' },
+{ id: '0JQ5DAqbMKFCfObibaOZbv', name: 'Gaming' },
+{ id: '0JQ5DAqbMKFLb2EqgLtpjC', name: 'Wellness' },
+{ id: '0JQ5DAqbMKFPw634sFwguI', name: 'EQUAL' },
+{ id: '0JQ5DAqbMKFFzDl7qN9Apr', name: 'Chill' },
+{ id: '0JQ5DAqbMKFAXlCG6QvYQ4', name: 'Workout' },
+{ id: '0JQ5DAqbMKFDXXwE9BDJAr', name: 'Rock' },
+{ id: '0JQ5DAqbMKFA6SOHvT3gck', name: 'Party' },
+{ id: '0JQ5DAqbMKFCbimwdOYlsl', name: 'Focus' },
+{ id: '0JQ5DAqbMKFCuoRTxhYWow', name: 'Sleep' },
+{ id: '0JQ5DAqbMKFRY5ok2pxXJ0', name: 'Cooking & Dining' },
+{ id: '0JQ5DAqbMKFAJ5xb0fwo9m', name: 'Jazz' },
+{ id: '0JQ5DAqbMKFEZPnFQSFB1T', name: 'R&B' },
+{ id: '0JQ5DAqbMKFAUsdyVjCQuL', name: 'Romance' },
+{ id: '0JQ5DAqbMKFIpEuaCnimBj', name: 'Soul' },
+{ id: '0JQ5DAqbMKFCWjUTdzaG0e', name: 'Indie' },
+{ id: '0JQ5DAqbMKFPrEiAOxgac3', name: 'Classical' },
+{ id: '0JQ5DAqbMKFx0uLQR2okcc', name: 'At Home' },
+{ id: '0JQ5DAqbMKFDkd668ypn6O', name: 'Metal' },
+{ id: '0JQ5DAqbMKFFoimhOqWzLB', name: 'Kids & Family' },
+{ id: '0JQ5DAqbMKFxXaXKP7zcDp', name: 'Latin' },
+{ id: '0JQ5DAqbMKFObNLOHydSW8', name: 'Caribbean' },
+{ id: '0JQ5DAqbMKFQiK2EHwyjcU', name: 'Blues' },
+{ id: '0JQ5DAqbMKFAjfauKLOZiv', name: 'Punk' },
+{ id: '0JQ5DAqbMKFKLfwjuJMoNC', name: 'Country' },
+{ id: '0JQ5DAqbMKFy78wprEpAjl', name: 'Folk & Acoustic' },
+{ id: '0JQ5DAqbMKFNQ0fGp4byGU', name: 'Afro' },
+{ id: '0JQ5DAqbMKFAQy4HL4XU2D', name: 'Travel' },
+{ id: '0JQ5DAqbMKFRieVZLLoo9m', name: 'Instrumental' },
+{ id: '0JQ5DAqbMKFGvOw3O4nLAf', name: 'K-Pop' },
+{ id: '0JQ5DAqbMKFLjmiZRss79w', name: 'Ambient' },
+{ id: '0JQ5DAqbMKFF1br7dZcRtK', name: 'Pride' },
+{ id: '0JQ5DAqbMKFLVaM30PMBm4', name: 'Summer' }]
+
 const api = new Spotify({
     clientId: 'fdddbaf4de844274a0e20ed6f11a45dd',
     clientSecret: 'd25b808d48da4bcf88ed309770f49ab7',
     redirectUri: 'http://localhost:3000/callback'
 });
-const excode = 'BQCG0tc5lFSgTVTmLZ0caKqMayTM0iPtcYPPijFXhl8qP_9PtbUF9MCAmRGJZAf95xjsbszNnsOE0qDEk34t_46Wp9GebQiYkeks0dKdzX4anE-tMeu64pTDVxmD_H1Otxf6jdGsoy-_Q6pcvisA9S7nz2Isb9UxOv1fOOv_TKhLVWtNbYOzm_6mhBRaEKZA_2vtxlVaMcl5UT5NnfHNuiPT0g'
+const excode = 'BQDFEog7hIUcmYcbUB3HL5V7-tz4VJVTexdUufyjwjh3h8tBFmflPJi2X9VXHO_HOZ0vOJiRitWXnuLwmrqXSBk0xxaNwQ7Bq8wiTrdWYvin2BqThncUObK3DipIEox_uhHMRn_3eTgZJ_ksSeuwpddM-SzDiuo0xVxOv6mBuFHQZkGRxkcCB0qzY4iT6Qs_XhA1EmtXUFhKpNHtu0lct6Vksg'
 
 const formattrack = (res) => {
     return {
@@ -221,37 +260,17 @@ app.get('/featured', async (req, res) => {//featured playlists for GR
     res.json(data.body);
 });
 
-app.get('/categorypls', async (req, res) => {//get category ids and then playlists
+app.get('/categorypls', async (req, res) => {//get category playlists with above ready names and ids
     api.setAccessToken(excode);
-    const catsdata = await api.getCategories({ locale: 'GR', limit: 50 });
-    console.log(catsdata);
-    const cats = [];
-    catsdata.body.categories.items.forEach(el => {
-        cats.push({
-            id: el.id,
-            name: el.name
-        });
-    });
     const final = [];
-    cats.forEach(async el => {
-        const data = await api.getPlaylistsForCategory('0JQ5DAqbMKFQ00XGBls6ym');
-        console.log(data);
-        const pls = [];
-        data.body.playlists.items.forEach(el => {
-            pls.push({
-                name: el.name,
-                owner: el.owner.display_name,
-                image: el.images[0].url,
-                id: el.id
-            });
-        });
-        final.push({
-            name: el.name,
-            id: el.id,
-            pls: pls
-        });
-    });
-    res.json(catsdata);
+    const pls = [];
+    for (let i = 0; i < categories.length; i++) {
+        const cat = categories[i];
+        const data = await api.getPlaylistsForCategory(cat.id, { limit: 20 });
+        const playlists = data.body.playlists.items;
+        final.push({ name: cat.name, pls: playlists });
+    }
+    res.json(pls);
 });
 
 app.get('/download/:id', async (req, res) => {
